bb472ebb251cd409a590e37ec0316da34ab24e17
David Feltell <david.feltell@foundry.com>
2022-03-07 17:37:58 +0000

    squash: Use constants for internal BlobTrait dict queries
    Signed-off-by: David Feltell <david.feltell@foundry.com>

--- src/openassetio-core/specification/trait/BlobTrait.cpp
+++ src/openassetio-core/specification/trait/BlobTrait.cpp
@@ -9,18 +9,23 @@ namespace openassetio {
 inline namespace OPENASSETIO_VERSION {
 namespace specification::trait {
 
+namespace {
+const property::Key kUrl = "url";
+const property::Key kMimeType = "mimeType";
+}  // namespace
+
 property::Maybe<property::Str> BlobTrait::getUrl() const {
-  return data()->getTraitProperty<property::Str>(kId, "url");
+  return data()->getTraitProperty<property::Str>(kId, kUrl);
 }
 
-void BlobTrait::setUrl(property::Str url) { data()->setTraitProperty(kId, "url", std::move(url)); }
+void BlobTrait::setUrl(property::Str url) { data()->setTraitProperty(kId, kUrl, std::move(url)); }
 
 [[maybe_unused]] property::Maybe<property::Str> BlobTrait::getMimeType() const {
-  return data()->getTraitProperty<property::Str>(kId, "mimeType");
+  return data()->getTraitProperty<property::Str>(kId, kMimeType);
 }
 
 void BlobTrait::setMimeType(property::Str mimeType) {
-  data()->setTraitProperty(kId, "mimeType", std::move(mimeType));
+  data()->setTraitProperty(kId, kMimeType, std::move(mimeType));
 }
 
 }  // namespace specification::trait
